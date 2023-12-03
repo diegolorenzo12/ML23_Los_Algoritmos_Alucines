fae069a64e9bbf3c207daec9bbb1656339ae2836
Cesar <crenteriaejr@gmail.com>
2023-12-03 01:47:51 -0800

    fixed infinite recursion

--- ml23/proyecto_final/network.py
+++ ml23/proyecto_final/network.py
@@ -18,12 +18,19 @@ class DeepQNetwork(nn.Module):
         self.loss = nn.MSELoss()
         self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
         self.to(self.device)
-        self.target = DeepQNetwork(lr, input_dims, fc1_dims, fc2_dims, n_actions)
-        self.target.load_state_dict(self.state_dict())
-        self.target.eval()
+
+        self.target = TargetNetwork(lr, input_dims, fc1_dims, fc2_dims, n_actions)
+        self.update_target_network()
 
     def update_target_network(self):
-        self.target.load_state_dict(self.state_dict())
+        q_dict = self.state_dict()
+        target_dict = self.target.state_dict()
+
+        for key in q_dict.keys():
+            if key.startswith('fc'):
+                target_dict[key] = q_dict[key]
+
+        self.target.load_state_dict(target_dict)
 
     def forward(self, state):
         x = F.relu(self.fc1(state))
@@ -112,3 +119,25 @@ class Agent():
 
     def load_model(self, filename):
         self.Q_eval.load_state_dict(T.load(filename))
+
+class TargetNetwork(nn.Module):
+    def __init__(self, lr, input_dims, fc1_dims, fc2_dims, n_actions):
+        super(TargetNetwork, self).__init__()
+        self.input_dims = input_dims
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.n_actions = n_actions
+        self.fc1 = nn.Linear(*self.input_dims, self.fc1_dims)
+        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
+        self.fc3 = nn.Linear(self.fc2_dims, self.n_actions)
+        self.optimizer = optim.Adam(self.parameters(), lr=lr)
+        self.loss = nn.MSELoss()
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+        self.to(self.device)
+
+    def forward(self, state):
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        actions = self.fc3(x)
+
+        return actions
